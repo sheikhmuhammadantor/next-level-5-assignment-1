d4ab663f40bcb9ea6d94d00bc5d0b59b8b975148
Sheikh Muhammad Antor <sheikhmuhammadantor@gmail.com>
2025-05-08 15:39:33 +0600

    Remove unnecessary comment lines for cleaner code

--- b5-assignment-1.ts
+++ b5-assignment-1.ts
@@ -2,14 +2,12 @@ function formatString(input: string, toUpper?: boolean): string {
   return toUpper ?? true ? input.toUpperCase() : input.toLowerCase();
 }
 
-//
 function filterByRating(
   items: { title: string; rating: number }[]
 ): { title: string; rating: number }[] {
   return items.filter((item) => item.rating >= 4);
 }
 
-//
 function concatenateArrays<T>(...arrays: T[][]): T[] {
   // return arrays.flat();
   const arr: T[] = [];
@@ -17,7 +15,6 @@ function concatenateArrays<T>(...arrays: T[][]): T[] {
   return arr;
 }
 
-//
 class Vehicle {
   private make: string;
   private year: number;
@@ -45,13 +42,11 @@ class Car extends Vehicle {
   }
 }
 
-//
 function processValue(value: string | number): number {
   if (typeof value === "string") return value.length;
   return value * 2;
 }
 
-//
 interface Product {
   name: string;
   price: number;
@@ -64,7 +59,6 @@ function getMostExpensiveProduct(products: Product[]): Product | null {
   );
 }
 
-//
 enum Day {
   Monday,
   Tuesday,
@@ -80,7 +74,6 @@ function getDayType(day: Day): string {
   return "Weekday";
 }
 
-//
 async function squareAsync(n: number): Promise<number> {
   return new Promise((resolve, reject) => {
     if (n >= 0) {
